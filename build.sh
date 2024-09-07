6f6e1a9cce2747f22d5676a5d78f24281b59b256
Wouter van Os <wouter0100@gmail.com>
2024-09-07 15:04:57 +0200

    feat: improved build script

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ RELEASE="$(rpm -E %fedora)"
 # Install earlyoom
 rpm-ostree install earlyoom
 systemctl enable earlyoom
+
+# Remove firefox
+rpm-ostree override remove firefox firefox-langpacks
+
+# Remove unnecessary packages
+rpm-ostree cleanup -m
+
+# Remove temporary files and caches
+rm -rf /var/cache/dnf /var/lib/dnf /tmp/* /var/tmp/*
